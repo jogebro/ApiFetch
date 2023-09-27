3ca773a9dc3eb4921d94c3524511db0f967ea3e0
jogebro <joelabril2110@gmail.com>
2023-09-27 13:04:27 -0500

    feat: :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{useEffect, useState} from 'react';
+const efecto = {
+  objectFit: "cover"
 }
+export default function App(){
+  const [isLoading, setIsLoading] = useState(true);
+  const [imageUrl, setImageUrl] = useState(null);
+  const [error, setError] = useState(null);
 
-export default App;
+  useEffect(()=>{
+      if(isLoading){
+        async function fetchData(){
+          try {
+            const response = await fetch("https://dog.ceo/api/breeds/image/random")
+            if(response.ok){
+              const dog = await response.json();
+              setImageUrl(dog.message);
+              setError(null);
+              setIsLoading(false);
+            }else{
+              setError("No hay ninguna imagen de perrito")
+            }
+          } catch (error) {
+            setError("El api no esta respondiendo");
+          }
+        }
+        fetchData();
+      }
+  },[isLoading])
+
+  const randomDog = ()=>{
+    setIsLoading(true)
+  }
+
+  if(isLoading){
+    return(
+      <div className='App'>
+        <h1>Cargando . . .</h1>
+      </div>
+    )
+  }
+
+  if(isLoading){
+    return(
+      <div className='App'>
+        <h1>{error}</h1>
+        <button onClick={randomDog}>Vuelve a intentar</button>
+      </div>
+    )
+  }
+
+  return(
+    <div className='App'>
+        <img src={imageUrl} alt='imagen de perrito' height="500px" width="500px" style={efecto}/>
+        <button onClick={randomDog}>Cambiar imagen</button>
+    </div>
+  )
+}
